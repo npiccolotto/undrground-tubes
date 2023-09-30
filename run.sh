801283f4b3521e9cd601aa721ff32aa188085097
Nikolaus Piccolotto <nikolaus.piccolotto@tuwien.ac.at>
2023-09-30 16:04:38 +0200

    fix

--- run.sh
+++ run.sh
@@ -31,4 +31,4 @@ dataset=$7
 jobname="esvis-$dataset-$support_type-$support_partition-$layouter-$overlapper-$router"
 
 python $base/render2.py --read-dir $base/data --write-dir $TMPDIR --support-type $support_type --support-partition $support_partition --layouter $layouter --overlap-remover $overlapper --router $router --read-dir $base/designspace-test --dataset $dataset
-cp -r $TMPDIR $base/$1
+cp -r $TMPDIR $1
