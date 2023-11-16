a75394721a32f459716dcb4bdcf1aa5a1890637c
Nikolaus Piccolotto <nikolaus.piccolotto@tuwien.ac.at>
2023-11-16 21:11:40 +0100

    switcheroo of support type

--- start_tasks.sh
+++ start_tasks.sh
@@ -22,11 +22,11 @@ for i in $(seq 1 10); do
   j=$((i-1));
   s=$((i*20));
   dataset="ds_dataset${j}_${s}";
-  for support_type in "steiner-tree" "path"; do
-    for layouter in "mds" "qsap"; do
-      for overlapper in "dgrid" "hagrid"; do
-        for connecter in "opt" "heuristic"; do
-          for router in "opt" "heuristic"; do
+  for layouter in "mds" "qsap"; do
+    for overlapper in "dgrid" "hagrid"; do
+      for connecter in "opt" "heuristic"; do
+        for router in "opt" "heuristic"; do
+          for support_type in "steiner-tree" "path"; do
             jobname="esvis-$dataset-$support_type-$layouter-$overlapper-$connecter-$router";
             echo "submitting $jobname";
             qsub -N $jobname -l bc3 -l longrun=1 -l mem_free=$mem -l h_vmem=$mem -l h_rt=$hlr -e $base/logs/ -o $base/logs/ -r y run.sh $base/$jobname $support_type $layouter $overlapper $connecter $router $dataset
