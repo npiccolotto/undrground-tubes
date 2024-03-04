6cc0c995d06ea4f9194145fff15890b54b9463bf
Nikolaus Piccolotto <nikolaus.piccolotto@tuwien.ac.at>
2024-03-04 15:59:13 +0100

    generate all the datasets!

---
+++ run_surveygen.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source /home1/npiccolotto/.bashrc
+
+base='/home1/npiccolotto/ensemble-sets/code'
+# adapt for local use
+#base='/Users/npiccolotto/Projects/cvast/bssvis/ensemble-set-rendering'
+#export TMPDIR='/tmp/garbage'
+
+# test that all the binaries exist: pyenv, loom
+if ! command -v pyenv 1>/dev/null 2>&1; then
+  echo "pyenv not installed, can't continue." >&2
+  exit 1
+fi
+
+if ! command -v loom 1>/dev/null 2>&1; then
+  echo "loom not installed, can't continue." >&2
+  exit 1
+fi
+
+# set python version
+pyenv local 3.10
+
+# set gurobi stuff
+export GRB_LICENSE_FILE="/home1/share/gurobi/gurobi.lic"
+export GUROBI_HOME="/home1/share/gurobi/gurobi/linux64"
+
+dataset="$2"
+strategy="$3"
+weight="$4"
+
+jobname="esvis-$dataset-$weight-$support_type-$layouter-$overlapper-$connecter-$router";
+
+cd $base
+python render2.py --strategy $strategy --write-dir $TMPDIR --overlap-remover dgrid --connecter auto --router auto --read-dir $base/designspace-test --dataset $dataset --weight $weight --connect-objective joint -serialize-graph 0 --compute-metrics 0
+cp -r $TMPDIR $1
---
+++ start_tasks_surveygen.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# set the base directory
+base='/home1/npiccolotto/ensemble-sets/results'
+
+# set hard memory constraint
+#hm=8G
+
+# set required free memory
+#rfm=8G
+
+# set soft memory (not used)
+#sm=7.8G
+
+mem=32G
+
+# set hard limit run time (seconds)
+hlr=18000 # 4 calls to gurobi, 1h for each plus one hour for the rest = 5h
+
+
+for i in $(seq 1 24); do
+  j=$((i-1));
+  dataset="ds_dataset${j}";
+  for strategy in "heuristic" "opt"; do
+    for weight in "0" "0.5" "1"; do
+      jobname="essurvey-$dataset-$strategy-$weight";
+      echo "submitting $jobname";
+      qsub -N $jobname -l bc4 -l mem_free=$mem -l h_vmem=$mem -l h_rt=$hlr -e $base/logs/ -o $base/logs/ -r y run.sh $base/$jobname $dataset $strategy $weight
+    done
+  done
+done
+
