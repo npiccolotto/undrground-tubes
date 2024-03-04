34bfc2885c43c55c57f003a3584a1b31fa2a9071
Nikolaus Piccolotto <nikolaus.piccolotto@tuwien.ac.at>
2024-03-04 16:21:10 +0100

    typo

--- run_surveygen.sh
+++ run_surveygen.sh
@@ -29,8 +29,8 @@ dataset="$2"
 strategy="$3"
 weight="$4"
 
-jobname="esvis-$dataset-$weight-$support_type-$layouter-$overlapper-$connecter-$router";
+jobname="essurvey-$dataset-$strategy-$weight";
 
 cd $base
-python render2.py --strategy $strategy --write-dir $TMPDIR --overlap-remover dgrid --connecter auto --router auto --read-dir $base/designspace-test --dataset $dataset --weight $weight --connect-objective joint -serialize-graph 0 --compute-metrics 0
+python render2.py --strategy $strategy --write-dir $TMPDIR --overlap-remover dgrid --connecter auto --router auto --read-dir $base/designspace-test --dataset $dataset --weight $weight --connect-objective joint --serialize-graph 0 --compute-metrics 0
 cp -r $TMPDIR $1
