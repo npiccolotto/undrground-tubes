1d9099f110d604b70796411f0bd3af4d41fc4c8b
Nikolaus Piccolotto <nikolaus.piccolotto@tuwien.ac.at>
2024-04-24 19:30:21 +0200

    save

--- eval/qual-analysis.R
+++ eval/qual-analysis.R
@@ -105,3 +105,25 @@ print(d)
 
 '5 better than 1 ***, 0 better than 1 **, no difference between 0 and 5'
 
+# Check correlation between confidence and accuracy
+acc <- read.csv('survey_long.csv',colClasses = c("numeric", "numeric", "factor", "factor", "factor", "factor"),)
+
+qual_subset <- qual[qual$metric == 'confidence',]
+qual_subset <- dplyr::arrange(qual_subset, participant, task, alpha)
+
+acc_subset <- acc[acc$metric=='a' & acc$pipeline=='h',]
+acc_subset <- dplyr::arrange(acc_subset, participant, task, alpha)
+
+cor(qual_subset$rating, acc_subset$value, method='spearman')
+
+
+# Check correlation between confidence and preference - are these the same answers?
+qual_subset1 <- qual[qual$metric == 'preference',]
+qual_subset1 <- dplyr::arrange(qual_subset1, participant, task, alpha)
+
+qual_subset2 <- qual[qual$metric == 'confidence',]
+qual_subset2 <- dplyr::arrange(qual_subset2, participant, task, alpha)
+
+cor(qual_subset1$rating, qual_subset2$rating, method='spearman')
+
+'kind of'
