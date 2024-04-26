f0c63a76d44b8742094da97c87b8d8ed548ac840
Nikolaus Piccolotto <nikolaus.piccolotto@tuwien.ac.at>
2024-04-26 08:05:01 +0200

    more plots

--- eval/plots.R
+++ eval/plots.R
@@ -52,7 +52,7 @@ labeller_sqrt_inv <- function(breaks) {
 labeller_sqrt(c(0,1,2,3))
 labeller_sqrt_inv(1:4)
 
-
+## Plots using transformed time
 ggplot(dataset[dataset$task == 'T1' | dataset$task == 'T4',]) +
   aes(
     y = factor(alpha),
@@ -107,6 +107,7 @@ ggplot(dataset[dataset$task == 'T2' | dataset$task == 'T3',]) +
   scale_fill_discrete() +
   facet_grid(scales = 'free_x', rows = 'task')
 
+## Accuracy Plot
 dataset_a <- read.csv('survey_long.csv')
 dataset_a <- dataset_a[dataset_a$metric == 'a', ]
 dataset_a[dataset_a$alpha == 5, ]$alpha <- 0.5
@@ -115,24 +116,23 @@ dataset_a[dataset_a$task == 2,]$task <- 'T2'
 dataset_a[dataset_a$task == 3,]$task <- 'T3'
 dataset_a[dataset_a$task == 4,]$task <- 'T4'
 
-
 ggplot(dataset_a) +
   aes(
-    y = factor(alpha),
-    x = value,
+    x = factor(alpha),
+    y = value,
     fill = factor(alpha),
     group = task
   ) +
-  scale_x_continuous(breaks=seq(0,1,.25))+
+  scale_y_continuous(breaks=seq(0,1,.25))+
   stat_histinterval(
-    orientation='horizontal',
+    orientation='vertical',
     breaks = seq(0, 1, 0.1),
     point_interval = 'median_qi',
     slab_alpha=.5,
     .width = c(.5, .75),
     position = 'dodgejust',
-   # normalize = 'panels',
-    side='right'
+    normalize = 'panels',
+    side='both'
   ) +
   scale_fill_discrete() +
   theme_bw()+
@@ -143,6 +143,45 @@ ggplot(dataset_a) +
     axis.title.y = element_text(size=18),
     strip.text = element_text(size = 18)
   )+
-  xlab('F1 Score')+
-  ylab('Weight')+
-  facet_grid(rows = 'task')
+  ylab('F1 Score')+
+  xlab('Weight')+
+  facet_grid(cols=vars(task))
+
+## Plot using measured time
+dataset <- read.csv('survey_long.csv')
+dataset[dataset$metric == 't', ]$value <-
+  dataset[dataset$metric == 't', ]$value / 1000
+dataset <- dataset[dataset$metric == 't', ]
+dataset[dataset$alpha == 5, ]$alpha <- 0.5
+dataset[dataset$task == 1,]$task <- 'T1'
+dataset[dataset$task == 2,]$task <- 'T2'
+dataset[dataset$task == 3,]$task <- 'T3'
+dataset[dataset$task == 4,]$task <- 'T4'
+
+ggplot(dataset) +
+  aes(
+    x = factor(alpha),
+    y = value,
+    fill = factor(alpha),
+    group = factor(task)
+  ) +
+  stat_eye(
+    point_interval = 'mean_hdci',
+    slab_alpha=.5,
+    normalize='panels',
+    .width = c(.5, .75),
+    position = 'dodgejust'
+  ) +
+  theme_bw()+
+  theme(
+    legend.position='none',
+    axis.text = element_text(size=14),
+    axis.title.x = element_text(size=18),
+    axis.title.y = element_text(size=18),
+    strip.text = element_text(size = 18)
+  )+
+  xlab('Weight')+
+  ylab('Seconds')+
+  scale_y_continuous(limits = c(0,90),breaks=c(0,10,20,30,45,60,90))+
+  scale_fill_discrete() +
+  facet_grid(scales = 'free_x', cols = vars(task))
